388c5c3fa7bc8d7279d41e9519876122f20fd134
[PUZ]Cklebepand <116970894+nagibator3000-228@users.noreply.github.com>
2023-07-27 22:11:19 +0200

    cors test

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const cors = require("cors");
-
-app.use(cors({origin: 'https://example.com'}));
 
 // Отслеживание url адреса и отображение нужной HTML страницы
 
